c61edb1b93b5b6162ce5f81fb45a90c1f373862f
Seth-Keenan <seth.j.keenan@gmail.com>
2024-02-01 10:50:49 -0600

    First commit

---
+++ src/edu/mu/Main.java
@@ -0,0 +1,13 @@
+package edu.mu;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Name: Dorion Massengill, gitID: 89409962");
+		System.out.println("Name: Paul Maschhoff, gitID: 116123387");
+		System.out.println("Name: Seth Keenan, gitID: 119908315");
+		
+	}
+
+}
